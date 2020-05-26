6b0fc24d76cf959d3306df35229f1e684a42d3c0
lamlaaaam <lamzhiyuan98@gmail.com>
2020-05-26 14:54:10 +0800

    Joystick controls added

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public Camera cam; // Main camera.
     public bool isMoving; // Flag for when player is moving.
     public Animator animator;
+    public Joystick moveStick, lookStick;
     private Vector2 movement, look; // Movement and looking direction vectors.
     private float currentSpeed;
 
@@ -17,26 +18,28 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         // For movement.
-        //movement.x = moveStick.Horizontal;
-        //movement.y = moveStick.Vertical;
-        //look.x = lookStick.Horizontal;
-        //look.y = lookStick.Vertical;
+        movement.x = moveStick.Horizontal;
+        movement.y = moveStick.Vertical;
+        isMoving = movement != Vector2.zero;
 
-        bool W = Input.GetKey("w");
-        bool A = Input.GetKey("a");
-        bool S = Input.GetKey("s");
-        bool D = Input.GetKey("d");
+        look.x = lookStick.Horizontal;
+        look.y = lookStick.Vertical;
 
-        if (!W && !A && !S && !D) { movement = Vector2.zero; isMoving = false; }
-        else { isMoving = true; }
+        //bool W = Input.GetKey("w");
+        //bool A = Input.GetKey("a");
+        //bool S = Input.GetKey("s");
+        //bool D = Input.GetKey("d");
 
-        if (W) { movement.y = 1; }
-        if (S) { movement.y = -1; }
-        if (D) { movement.x = 1; }
-        if (A) { movement.x = -1; }
+        //if (!W && !A && !S && !D) { movement = Vector2.zero; isMoving = false; }
+        //else { isMoving = true; }
+
+        //if (W) { movement.y = 1; }
+        //if (S) { movement.y = -1; }
+        //if (D) { movement.x = 1; }
+        //if (A) { movement.x = -1; }
 
         // For looking.
-        look = (Vector2) cam.ScreenToWorldPoint(Input.mousePosition) - rb.position;
+        //look = (Vector2) cam.ScreenToWorldPoint(Input.mousePosition) - rb.position;
     }
 
     void FixedUpdate()
--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerShooting : MonoBehaviour
 {
@@ -6,22 +7,33 @@ public class PlayerShooting : MonoBehaviour
     public Transform firePoint; // Point where bullet is spawned.
     public Animator animator;
     public MuzzleFlash flash;
+    public Button shoot;
     public float force, recoilForce; // Bullet's force and recoil.
+
+    void Start()
+    {
+        shoot.onClick.AddListener(Shoot);
+    }
 
     void Update()
     {
         // Shoot on left click.
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Muzzle flash.
-            flash.Flash(5, 0.05f);
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Shoot();
+        //}
+    }
 
-            GameObject spawnedBullet = Instantiate(bullet, firePoint.position, Quaternion.identity); // Spawn bullet.
-            Rigidbody2D rb = spawnedBullet.GetComponent<Rigidbody2D>(); // Get bullet's RB.
-            rb.AddForce(transform.up * force);
+    void Shoot()
+    {
+        // Muzzle flash.
+        flash.Flash(5, 0.05f);
 
-            // Recoil.
-            gameObject.GetComponent<Rigidbody2D>().AddForce(-transform.up * recoilForce * Time.deltaTime);
-        }
+        GameObject spawnedBullet = Instantiate(bullet, firePoint.position, Quaternion.identity); // Spawn bullet.
+        Rigidbody2D rb = spawnedBullet.GetComponent<Rigidbody2D>(); // Get bullet's RB.
+        rb.AddForce(transform.up * force);
+
+        // Recoil.
+        gameObject.GetComponent<Rigidbody2D>().AddForce(-transform.up * recoilForce * Time.deltaTime);
     }
 }
