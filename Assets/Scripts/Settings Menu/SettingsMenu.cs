e96a5e4a093072a71cc4c9e411c257f2c2b7746d
lamlaaaam <lamzhiyuan98@gmail.com>
2020-07-07 12:15:13 +0800

    Settings menu

---
+++ Assets/Scripts/Settings Menu/SettingsMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        Debug.Log(volume);
+    }
+
+    public void SetQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
+    public void Hide()
+    {
+        CanvasGroup group = GetComponent<CanvasGroup>();
+        group.alpha = 0f;
+        group.interactable = false;
+        group.blocksRaycasts = false;
+    }
+
+    public void Show()
+    {
+        CanvasGroup group = GetComponent<CanvasGroup>();
+        group.alpha = 1f;
+        group.interactable = true;
+        group.blocksRaycasts = true;
+    }
+}
